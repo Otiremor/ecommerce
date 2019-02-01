1969e92106c515f00534e7db3748057bb19ad1c2
Otiremor <romero.ito@hotmail.com>
2019-02-01 06:34:15 -0200

    Tela de login

--- vendor/hcodebr/php-classes/src/Page.php
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -7,10 +7,14 @@ class Page {
     private $tpl;
     private $options = [];
     private $defaults = [
+        "header" => true,
+        "footer" => true,
         "data" => []
     ];
     
     public function __construct($opts = array(), $tpl_dir = "/views/"){
+        //$this->defaults["data"]["session"] = $_SESSION;
+        
         $this->options = array_merge($this->defaults, $opts);
         // config
         $config = array(
@@ -25,7 +29,7 @@ class Page {
         
         $this->setData($this->options["data"]);
         
-        $this->tpl->draw("header");
+        if ($this->options["header"] === true) $this->tpl->draw("header");
     }
     
     private function setData($data = array())
@@ -43,7 +47,7 @@ class Page {
     }
     
     public function __destruct(){
-        $this->tpl->draw("footer");
+        if ($this->options["header"] === true) $this->tpl->draw("footer");
     }
 }
 ?>
