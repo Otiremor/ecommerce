5797cf4eedb1b44d793d83397aeda720d09a389f
Otiremor <romero.ito@hotmail.com>
2019-02-06 07:01:36 -0200

    Admin - Categorias

--- vendor/hcodebr/php-classes/src/Model.php
+++ vendor/hcodebr/php-classes/src/Model.php
@@ -1,36 +1,39 @@
 <?php
 namespace Hcode;
 
-class Model {
+class Model
+{
+
     private $values = [];
-    
+
     public function __call($name, $args)
     {
         $method = substr($name, 0, 3);
         $fieldName = substr($name, 3, strlen($name));
         
-        /*var_dump($method, $fieldName);        
-        exit();*/
+        /*
+         * var_dump($method, $fieldName);
+         * exit();
+         */
         
-        switch ($method)
-        {
+        switch ($method) {
             case "get":
-                return $this->values[$fieldName];
-            break;
+                return (isset($this->values[$fieldName])) ? $this->values[$fieldName] : NULL;
+                break;
             
             case "set":
                 return $this->values[$fieldName] = $args[0];
-            break;
+                break;
         }
     }
-    
+
     public function setData($data = array())
     {
         foreach ($data as $key => $value) {
             $this->{"set" . $key}($value);
         }
     }
-    
+
     public function getValues()
     {
         return $this->values;
